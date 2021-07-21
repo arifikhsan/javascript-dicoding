dd5574595f5fba23d471916522f1013452e36e35
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2021-07-21 14:33:33 +0700

    operator

---
+++ operator/shortcut.js
@@ -0,0 +1,8 @@
+let x = 10;
+let y = 5;
+
+console.log(x += y); // artinya -> x = x + y;
+console.log(x -= y); // artinya -> x = x - y;
+console.log(x *= y); // artinya -> x = x * y;
+console.log(x /= y); // artinya -> x = x / y;
+console.log(x %= y); // artinya -> x = x % y;
