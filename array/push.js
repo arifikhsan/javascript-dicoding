1bafdc1943ddb20fc0f41e43297e62f6a5e103ff
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2021-07-21 16:39:24 +0700

    array

---
+++ array/push.js
@@ -0,0 +1,8 @@
+const myArray = ["Coklat", 42.5, 22, true, "Programming"];
+
+myArray.push("JavaScript");
+console.log(myArray);
+
+/* output
+[ 'Coklat', 42.5, 22, true, 'Programming', 'JavaScript' ]
+ */
