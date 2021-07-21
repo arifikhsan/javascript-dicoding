01acbf8cee57b31a027bed025e0f946d5c4bf95e
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2021-07-21 19:06:56 +0700

    function

---
+++ function/arrow.js
@@ -0,0 +1,37 @@
+const sayName = (name) => {
+  console.log(`Nama saya ${name}`);
+};
+
+sayName("Leia");
+
+/* output
+Nama saya Leia
+ */
+
+const sayHello = () => {
+  console.log("Selamat pagi semuanya!");
+};
+
+sayHello();
+
+/* output
+Selamat pagi semuanya!
+ */
+
+const sayNamee = (name) => console.log(`Nama saya ${name}`);
+sayNamee("Leia");
+
+const sayHelloo = () => console.log("Selamat pagi semuanya!");
+sayHelloo();
+
+/* output
+Nama saya Leia
+Selamat pagi semuanya!
+ */
+
+const multiply = (a, b) => a * b;
+console.log(multiply(3, 4));
+
+/* output
+12
+ */
